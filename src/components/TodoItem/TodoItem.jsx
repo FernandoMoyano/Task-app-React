bf6ece31f9091447e18c36b625884a571f095b30
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-14 17:10:36 -0300

    ternary operator in TodoItem

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,14 +1,21 @@
-import "./TodoItem.css"
+import "./TodoItem.css";
 /* eslint-disable react/prop-types */
 const TodoItem = (props) => {
   return (
     <div>
       <li className="todoItem">
-        <span className="icon icon-check icon-chek--active">
-          ✅{props.completed}
+        <span
+          className={`icon icon-check 
+        ${props.completed && "icon-check--active"}`}
+        >
+          V{props.completed}
         </span>
-        <p className="todoItem-p todoItem-p--complete">{props.text}</p>
-        <span className="icon icon-delete">❌</span>
+        <p
+          className={`todoItem-p ${props.completed && "todoItem-p--complete"}`}
+        >
+          {props.text}
+        </p>
+        <span className="icon icon-delete">X</span>
       </li>
     </div>
   );
