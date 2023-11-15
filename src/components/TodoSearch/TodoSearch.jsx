ffe7a329c3f5783346cd3ff14223b22dbe3c937e
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-14 21:29:59 -0300

    searchValue-setSearchValue

--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -1,8 +1,17 @@
-import "./TodoSearch.css"
+import { useState } from "react";
+import "./TodoSearch.css";
 const TodoSearch = () => {
-  return(
-    <input className="todoSearch" placeholder="Hacer tarea" value="Hacer Tarea" />
-  ) 
+  const [searchValue, setSearchValue] = useState("");
+  return (
+    <input
+      className="todoSearch"
+      placeholder="Hacer tarea"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
 };
 
 export default TodoSearch;
