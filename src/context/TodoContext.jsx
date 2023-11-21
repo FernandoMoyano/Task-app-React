3fe4b1d9fafe36bc5ec01f7c726eebe38e33da5b
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-21 18:28:29 -0300

    add Context

---
+++ src/context/TodoContext.jsx
@@ -0,0 +1,51 @@
+import { createContext, useState } from "react"
+import useLocalStorage from "../Hooks/useLocalStorage";
+
+const TodoContext=createContext()
+const TodoProvider = () => {
+   /* Estados */
+   const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_V1", []);
+  const [searchValue, setSearchValue] = useState("");
+
+  /* Estados derivados de todos */
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+
+  const totalTodos = todos.length;
+
+  /* Busqueda filtrada a partír del texto */
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  /* Marcar o desmarcar tarea como completada */
+  const handleComplete = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    /* Guardamos cambios en localStorage */
+    saveTodos(newTodos);
+  };
+
+  /* Eliminar tarea */
+  const handleDelete = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+    newTodos.splice(todoIndex, 1);
+    /* Guardamos cambios en localStorage */
+    saveTodos(newTodos);
+  };
+  return (
+    <TodoContext.Provider>
+
+    </TodoContext.Provider>
+  )
+}
+
+export {TodoContext,TodoProvider}
