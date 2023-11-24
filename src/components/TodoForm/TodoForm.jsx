43f5da79bbe5a7a58a4e4ac7708bf005aba4f81f
FernandoMoyano <fernandomoyano21@gmail.com>
2023-11-23 21:37:13 -0300

    newTodoValue-setNewTodoValue

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,8 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./TodoForm.css";
 import { TodoContext } from "../../context/TodoContext";
 const TodoForm = () => {
   const { setOpenModal } = useContext(TodoContext);
+  const [newTodoValue, setNewTodoValue] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -12,10 +13,19 @@ const TodoForm = () => {
   const handleCancelSubmit = () => {
     setOpenModal(false);
   };
+
+  const handleChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+
   return (
     <form>
       <label>Escribe un nuevo Todo</label>
-      <textarea placeholder="Hacer ejercicio"></textarea>
+      <textarea
+        placeholder="Hacer ejercicio"
+        value={newTodoValue}
+        onChange={handleChange}
+      ></textarea>
       <div className="todoForm-buttonContainer">
         <button
           onClick={handleSubmit}
